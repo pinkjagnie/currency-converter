3837d645a7e2e39b76561a809b523e5d4eb8837e
pinkjagnie <olga.krzysztofik@gmail.com>
2022-10-03 23:53:53 +0200

    Builed simple local API

--- App/index.js
+++ App/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import Navigation from './config/Navigation';
 
+import { api } from './util/api';
+
+api("/latest?base=USD")
+  .then(res => console.log(res))
+  .catch(err => console.log("err", err));
+
 export default function App()
  {
   return (
