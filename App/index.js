7caee5ce93f14c5e0eec45e4b01655d3174ace2d
pinkjagnie <olga.krzysztofik@gmail.com>
2022-10-03 19:01:43 +0200

    Configured stack Navigation

--- App/index.js
+++ App/index.js
@@ -1,31 +1,10 @@
 import React from 'react';
 
-import Home from "./screens/Home";
+import Navigation from './config/Navigation';
 
 export default function App()
  {
   return (
-  <Home />)
+    <Navigation />
+  )
 };
-
-
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
