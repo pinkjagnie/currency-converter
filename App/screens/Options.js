baa8a00951faed2cf731dccca3f2dc705ad146bd
pinkjagnie <olga.krzysztofik@gmail.com>
2022-10-03 12:18:50 +0200

    Displaying icons in Options

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
 
+import { Entypo } from "@expo/vector-icons";
+
 import colors from "../constans/colors";
 
 export default () => {
@@ -9,18 +11,21 @@ export default () => {
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>Themes</Text>
+        <Entypo name="chevron-right" size={20} color={colors.blue}/>
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native Basics</Text>
+        <Entypo name="export" size={20} color={colors.blue}/>
       </TouchableOpacity>
 
       <View style={styles.separator} />
 
       <TouchableOpacity style={styles.row}>
         <Text style={styles.text}>React Native by Example</Text>
+        <Entypo name="export" size={20} color={colors.blue}/>
       </TouchableOpacity>
 
     </SafeAreaView>
@@ -31,6 +36,9 @@ const styles = StyleSheet.create({
   row: {
     paddingHorizontal: 20,
     paddingVertical: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
   text: {
     fontSize: 16,
