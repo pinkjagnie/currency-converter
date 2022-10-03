4e518c241752e81456aca1370ab5ebe267b634de
pinkjagnie <olga.krzysztofik@gmail.com>
2022-10-04 00:51:53 +0200

    Getting rates on App launch

--- App/util/ConversionContext.js
+++ App/util/ConversionContext.js
@@ -1,12 +1,15 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 import { api } from "./api";
 
 export const ConversionContext = createContext();
 
+const DEFAULT_BASE_CURRENCY = 'USD';
+const DEFAULT_QUOTE_CURRENCY = 'GBP';
+
 export const ConversionContextProvider = ({ children }) => {
-  const [baseCurrency, _setBaseCurrency] = useState("USD");
-  const [quoteCurrency, setQuoteCurrency] = useState("GBP");
+  const [baseCurrency, _setBaseCurrency] = useState(DEFAULT_BASE_CURRENCY);
+  const [quoteCurrency, setQuoteCurrency] = useState(DEFAULT_QUOTE_CURRENCY);
   const [date, setDate] = useState();
   const [rates, setRates] = useState({});
 
@@ -37,7 +40,12 @@ export const ConversionContextProvider = ({ children }) => {
     setQuoteCurrency,
     date,
     rates
-  }
+  };
+
+  useEffect(() => {
+    setBaseCurrency(DEFAULT_BASE_CURRENCY);
+    setQuoteCurrency(DEFAULT_QUOTE_CURRENCY);
+  }, []);
 
   return (
     <ConversionContext.Provider value={contextValue}>
