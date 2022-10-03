2ee566d158144d3f07dd8153e7a36407a7849891
pinkjagnie <olga.krzysztofik@gmail.com>
2022-10-03 12:04:05 +0200

    Basic setup for main view

---
+++ App/screens/Options.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import colors from "../constans/colors";
+
+export default () => {
+  return (
+    <SafeAreaView>
+
+      <TouchableOpacity style={styles.row}>
+        <Text style={styles.text}>Themes</Text>
+      </TouchableOpacity>
+
+      <View style={styles.separator} />
+
+      <TouchableOpacity style={styles.row}>
+        <Text style={styles.text}>React Native Basics</Text>
+      </TouchableOpacity>
+
+      <View style={styles.separator} />
+
+      <TouchableOpacity style={styles.row}>
+        <Text style={styles.text}>React Native by Example</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  row: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+  },
+  text: {
+    fontSize: 16,
+    color: colors.text,
+  },
+  separator: {
+    backgroundColor: colors.border,
+    height: StyleSheet.hairlineWidth,
+    marginLeft: 20,
+  }
+})
